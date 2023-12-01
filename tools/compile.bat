21ee12befcdd238867a620ccd345a7797df06d0d
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2023-12-01 17:06:26 -0500

    Release v2.0.0

--- tools/compile.bat
+++ tools/compile.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set ISOFile=..\input\Kururin Squash! [J].iso
+set ISOTargetSize=1459978240
 
 if not exist "%ISOFile%" (
 	echo [INFO] "%ISOFile%" was not found
@@ -8,6 +9,18 @@ if not exist "%ISOFile%" (
 	echo Exiting in 10 seconds..
 	C:\Windows\System32\timeout.exe /t 10 /nobreak >nul
 	exit /b 0
+)
+
+for %%F in ("%ISOFile%") do (
+    set "ISOSize=%%~zF"
+)
+
+if %ISOSize% neq %ISOTargetSize% (
+    echo [INFO] "%ISOFile%" was has an incorrect size
+    echo Are you using the uncompressed ROM?
+    echo Exiting in 10 seconds..
+    C:\Windows\System32\timeout.exe /t 10 /nobreak >nul
+    exit /b 0
 )
 
 echo [INFO] Compiling patches - Please wait..
